be4dc7b88faa2948e47df6c45e071531f637db03
Facundo Baquero <facundito.xB@gmail.com>
2024-08-03 16:13:01 -0300

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Info from './componentes/Info';
 
 function App() {
   return (
@@ -17,7 +18,9 @@ function App() {
         >
           Learn React
         </a>
+      <Info/>
       </header>
+
     </div>
   );
 }
